d1f637499750a4d6014612bceb281b549596904a
darkeclipz <larsrotgers@gmail.com>
2021-06-20 10:53:46 +0200

    stack based recursion

--- Solver/BacktrackSearcher.cs
+++ Solver/BacktrackSearcher.cs
@@ -16,6 +16,7 @@ namespace CspSolver.Solver
 
         private ConstraintPropagator ConstraintPropagator;
         private const int ALL_ASSIGNED = -1;
+        private const int UNASSIGN = -1;
 
         public BacktrackSearcher(CspModel model)
         {
@@ -65,6 +66,60 @@ namespace CspSolver.Solver
             }
 
             return state;
+        }
+
+        private SearchState SearchStackBasedRecursion()
+        {
+            var stack = new Stack<(int, int)>();
+            var dataStack = new Stack<(int, Propagation)>();
+
+            var id = NextVariable();
+            var values = OrderedValues(Model.Variables[id]);
+
+            foreach(var value in values/*.Reverse()*/)
+            {
+                stack.Push((id, value));  
+            }
+
+            while(stack.Count > 0)
+            {
+                (var var, var value) = stack.Pop();
+
+                if(value >= 0)
+                {
+                    var variable = Model.Variables[var];
+                    variable.Assign(value);
+                    Statistics.TotalAssigments++;
+                    var propagation = ConstraintPropagator.PropagateAssignment(variable, value);
+                    stack.Push((var, UNASSIGN));
+                    dataStack.Push((value, propagation));
+
+                    if (propagation.IsValid)
+                    {
+                        var nextVariable = NextVariable();
+                        if (nextVariable == ALL_ASSIGNED)
+                        {
+                            return SearchState.Satisfied;
+                        }
+
+                        var nextValues = Model.Variables[nextVariable].Domain.Values; // OrderedValues(Model.Variables[nextVariable]);
+
+                        foreach (var nextValue in nextValues/*.Reverse()*/)
+                        {
+                            stack.Push((nextVariable, nextValue));
+                        }
+                    }
+                }
+                else
+                {
+                    (var x, var prop) = dataStack.Pop();
+                    var variable = Model.Variables[var];
+                    variable.Unassign(x);
+                    ConstraintPropagator.UndoPropagation(prop);
+                }
+            }
+
+            return SearchState.Infeasible;
         }
 
         private SearchState Search()
